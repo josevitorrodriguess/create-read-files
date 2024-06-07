6185dc6cca813443abd00c6159253c3ba7dd3545
Jose <josevitorrodrigues17@gmail.com>
2024-06-07 19:30:25 -0300

    add func resume

--- main.go
+++ main.go
@@ -2,13 +2,72 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"sort"
+	"strings"
+
+	"github.com/jdkato/prose/v2"
 )
 
 func main() {
+	
 	read := readFile("C:\\Users\\Cristina\\Desktop\\create-read-files\\text.txt")
-	createFile(read)
-	fmt.Println("Texto transferido com sucesso!")
+	summary := resume(read)
+	createFile(summary)
+	
+	fmt.Println("Texto transferido e resumido com sucesso!")
+}
+
+
+type SentenceScore struct{
+	sentence string
+	score float64
+}
+
+
+func resume(text string) string {
+	// analisa o arquivo
+	doc, err := prose.NewDocument(text)
+	if err != nil {
+		log.Fatalf("analitcs text error %v", err)
+	}
+
+	// verificar frequencia de palavras
+	wordFreq := make(map[string]int)
+	for _, tok := range doc.Tokens() {
+		word := strings.ToLower(tok.Text)
+		if len(word) > 2 {
+			wordFreq[word]++
+		}
+	}
+
+	var sentenceScores []SentenceScore
+	for _, sent := range doc.Sentences() {
+		score := 0.0
+		words := strings.Fields(strings.ToLower(sent.Text))
+
+		for _, word := range words {
+			score += float64(wordFreq[word])
+		}
+		sentenceScores = append(sentenceScores, SentenceScore{sent.Text, score})
+	}
+
+	sort.Slice(sentenceScores, func(i, j int) bool {
+		return sentenceScores[i].score > sentenceScores[j].score
+	})
+
+	numSentences := 3
+	if len(sentenceScores) < 3 {
+		numSentences = len(sentenceScores)
+	}
+
+	summary := ""
+	for i := 0; i < numSentences; i++ {
+		summary += sentenceScores[i].sentence + "\n" // Add a newline character after each sentence
+	}
+
+	return summary
 }
 
 func readFile(path string) string {
@@ -33,3 +92,5 @@ func createFile(content string) {
 	}
 	fmt.Printf("Foram escritos %d bytes\n", nb)
 }
+
+
